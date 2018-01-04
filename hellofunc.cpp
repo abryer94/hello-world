d4abc53c3ffbfa9455a5f89dafa31c34dc2eec49
abryer94 <32651550+abryer94@users.noreply.github.com>
2018-01-04 12:19:03 -0500

    Create hellofunc.cpp
    C++ source code for Makefile testing

---
+++ hellofunc.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <hellomake.h>
+using namespace std;
+
+void myPrintHelloMake(void) {
+
+        cout << "If you're reading this, the Makefile worked as intended! \n";
+
+        return;
